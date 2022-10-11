233e18be32e98df38983b2afb5be6a2d8dd0f78c
Napbelle <oa_filatova@inbox.ru>
2022-10-11 23:08:47 +0300

    Проверяют длину комментария

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function checkComment (lineWidth) {
+  let minLength = 20;
+  let maxLength = 140;
+if (lineWidth > minLength && lineWidth < maxLength) {
+    return true
+} else {
+    return false
+}
+};
