4607335b0ac627a1ec2fb9d8c16abb77abea7994
Keks <keks@htmlacademy.ru>
2022-10-31 13:30:14 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -18,15 +18,6 @@ const comments = {
   MAX: 200,
 };
 
-const id = {
-  MIN: 1,
-  MAX: 25,
-};
-
-const url = {
-  MIN: 1,
-  MAX: 25,
-};
 
 const descriptions = [
   'Это солнце. Оно горит.',
@@ -41,9 +32,28 @@ const descriptions = [
   'Снег, горы, любовь',
   'Главное правило в горах - не есть желтый снег',
   'Утки, у-у-у',
+  'Красное, голубое, зеленое...',
+  'Как сказал когда-то сам знаете кто',
+  '34 октября',
+  'Из древности к нам приходят они в тумане!',
+  'Ай-копирайтер написал этот текст',
+  'Двенадцать месяцев из 356 дней',
+  'Два месяца до Нового года',
+  'Осень',
+  'Из 15 лет один день я не видел тебя',
+  'Цифровизация промышленности все глубже проникает в нашу реальность',
+  'Двести двенадцать',
+  'Шесть шестых и бантик сбоку',
+  'Бесплатный массаж от уютных котиков',
+  'Солнце и я, еще бы море не штормило',
+  'Никогда не разговаривайте с незнакомцами',
+  'Я обиделась',
+  'Совет просто огонь!',
+  'Что за гадость эта ваша заливная рыба',
+  'Какадебака - вот что это',
 ];
 
-const photosCount = 10;
+const photosCount = 25;
 
 const getRandomPositiveInteger = (a, b) => {
   if (a < 0 || b < 0) {
@@ -58,17 +68,17 @@ const getRandomPositiveInteger = (a, b) => {
 const getRandomArrayElement = (elements) =>
   elements[getRandomPositiveInteger(0, elements.length - 1)];
 
-const createPhotoDescription = () => ({
-  id: getRandomPositiveInteger(id.MIN, id.MAX),
-  url: `photos/${getRandomPositiveInteger(url.MIN, url.MAX)}.jpg`,
+const createPhotoDescription = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
   description: getRandomArrayElement(descriptions),
   likes: getRandomPositiveInteger(likes.MIN, likes.MAX),
   comments: getRandomPositiveInteger(comments.MIN, comments.MAX),
 });
 
-const PhotoDescription = Array.from(
-  { length: photosCount },
-  createPhotoDescription
+const getPhotoDescription = () => Array.from(
+  { length: photosCount }, (_, PhotoDescriptionIndex) =>
+    createPhotoDescription(PhotoDescriptionIndex + 1)
 );
 
-PhotoDescription();
+getPhotoDescription();
