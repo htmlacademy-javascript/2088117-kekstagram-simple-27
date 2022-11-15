3bb9b815beb7d5fa10fb4a1fc659aadf0d3ab66b
Keks <keks@htmlacademy.ru>
2022-11-15 12:09:34 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/form.js
@@ -0,0 +1,68 @@
+import {isEscapeKey} from './util.js';
+import {checkLineWidth} from './check-line.js';
+
+const photoForm = document.querySelector('.img-upload__form');
+const modalForm = photoForm.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const uploadFile = photoForm.querySelector('#upload-file');
+const cancelButton = modalForm.querySelector('#upload-cancel');
+const commentField = modalForm.querySelector('.img-upload__text');
+const textareaField = commentField.querySelector('.text__description');
+
+
+const pristine = new Pristine(commentField, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__text',
+  errorTextClass: 'img-upload__error-text',
+});
+
+
+const sendForm = function () {
+  photoForm.addEventListener('submit', (evt) => {
+    const isValid = pristine.validate();
+    const commentLength = textareaField.value;
+    const maxCommentLength = 140;
+    const isGood = checkLineWidth(commentLength, maxCommentLength);
+    if (!isValid || !isGood) {
+      evt.preventDefault();
+    }
+  });
+};
+
+
+const onModalFormEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    hideModalForm();
+  }
+};
+
+function showModalForm () {
+  modalForm.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onModalFormEscKeydown);
+  textareaField.value = '';
+}
+
+function hideModalForm () {
+  photoForm.reset();
+  pristine.reset();
+  modalForm.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onModalFormEscKeydown);
+}
+
+const showForm = function() {
+  uploadFile.addEventListener('change', () => {
+    showModalForm();
+  });
+};
+
+const hideForm = function() {
+  cancelButton.addEventListener('click', () => {
+    hideModalForm();
+  });
+};
+
+
+export {showForm, hideForm, sendForm};
---
+++ 6/js/main.js
@@ -0,0 +1,11 @@
+import {getPhotoDescription} from './data.js';
+import {renderPhotos} from './photo.js';
+import {showForm, hideForm, sendForm} from './form.js';
+
+
+getPhotoDescription();
+renderPhotos();
+sendForm();
+showForm();
+hideForm();
+
